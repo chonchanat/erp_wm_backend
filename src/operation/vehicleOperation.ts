c036c76da36257412ccc7bbacfabb5fadd3779ba
chonchanat <t.chonchanat@hotmail.com>
2023-11-23 10:27:39 +0700

    fix: ambiguous active in getVehicleLicensePlate

--- src/operation/vehicleOperation.ts
+++ src/operation/vehicleOperation.ts
@@ -27,7 +27,7 @@ export async function getVehicleLicensePlate(transaction: any) {
             FROM DevelopERP_ForTesting2..Vehicle V
             LEFT JOIN DevelopERP_ForTesting2..MasterCode M
             ON V.registration_province_code_id = M.code_id
-            WHERE active = 1
+            WHERE V.active = 1
         `)
 }
 
