6e195a2c21ad8c258b4895e2f462b3369c39f86d
chonchanat <t.chonchanat@hotmail.com>
2023-11-28 09:11:04 +0700

    update: COALESCE null to string

--- src/operation/deviceOperation.ts
+++ src/operation/deviceOperation.ts
@@ -40,10 +40,10 @@ export async function getDeviceData(transaction: any, device_id: string) {
                 gateway_port_code_id, 
                 sms_server_number_code_id, 
                 sms_message_center_code_id, 
-                sim_serial, 
-                mobile_number, 
+                COALESCE(sim_serial, '') AS sim_serial, 
+                COALESCE(mobile_number, '') AS mobile_number, 
                 sim_type_code_id, 
-                description
+                COALESCE(description, '') AS description
             FROM DevelopERP_ForTesting2..DeviceConfig
             WHERE device_id = @device_id
         `)
