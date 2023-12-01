09e69e760ac2e6c6be0cd4f2bc220560fc1f9828
chonchanat <t.chonchanat@hotmail.com>
2023-12-01 17:01:35 +0700

    update: new handeleError message

--- src/middleware/handleError.ts
+++ src/middleware/handleError.ts
@@ -5,27 +5,105 @@ function duplicateError(err: any, req: Request, res: Response, next: NextFunctio
     const message = err.originalError.message
 
     if (message.includes('UC_CustomerName') && message.includes('duplicate key')) {
-        res.status(409).json({ status: 0, message: "Cannot insert duplicate customer_name" })
+        res.status(409).json({
+            status: 0,
+            message: {
+                en: "Cannot insert duplicate customer name",
+                th: "มีชื่อลูกค้านี้อยู่ในระบบแล้ว",
+                ch: "",
+            },
+        })
     } else if (message.includes('UC_FleetName') && message.includes('duplicate key')) {
-        res.status(409).json({ status: 0, message: "Cannot insert duplicate fleet_name" })
+        res.status(409).json({ 
+            status: 0, 
+            message: {
+                en: "Cannot insert duplicate fleet name",
+                th: "มีชื่อกลุ่มยานยนต์นี้อยู่ในระบบแล้ว",
+                ch: "",
+            },
+        })
     } else if (message.includes('UC_FrameNo') && message.includes('duplicate key')) {
-        res.status(409).json({ status: 0, message: "Cannot insert duplicate frame_no" })
+        res.status(409).json({ 
+            status: 0, 
+            message: {
+                en: "Cannot insert duplicate frame no",
+                th: "มีหมายเลขตัวถังนี้อยู่ในระบบแล้ว",
+                ch: "",
+            },
+        })
     } else if (message.includes('UC_SerialId') && message.includes('duplicate key')) {
-        res.status(409).json({ status: 0, message: "Cannot insert duplicate serial_id" })
+        res.status(409).json({ 
+            status: 0, 
+            message: {
+                en: "Cannot insert duplicate serial id",
+                th: "มีรหัสชุดอุปกรณ์นี้อยู่ในระบบแล้ว",
+                ch: "",
+            },
+        })
     } else if (message.includes('UC_Address_Customer') && message.includes('duplicate key')) {
-        res.status(409).json({ status: 0, message: "Cannot insert duplicate value for customer and address" })
+        res.status(409).json({ 
+            status: 0, 
+            message: {
+                en: "Cannot insert duplicate value for customer and address",
+                th: "มีการเชื่อมโยงระหว่างลูกค้ากับที่อยู่นี้อยู่ในระบบแล้ว",
+                ch: "",
+            },
+        })
     } else if (message.includes('UC_Address_MasterCode') && message.includes('duplicate key')) {
-        res.status(409).json({ status: 0, message: "Cannot insert duplicate vlaue for address and mastercode" })
+        res.status(409).json({ 
+            status: 0, 
+            message: {
+                en: "Cannot insert duplicate vlaue for address and mastercode",
+                th: "มีการเชื่อมโยงระหว่างที่อยู่กับประเภทที่อยู่นี้อยู่ในระบบแล้ว",
+                ch: "",
+            },
+        })
     } else if (message.includes('UC_Address_Person') && message.includes('duplicate key')) {
-        res.status(409).json({ status: 0, message: "Cannot insert duplicate value for address and person" })
+        res.status(409).json({ 
+            status: 0, 
+            message: {
+                en: "Cannot insert duplicate value for address and person",
+                th: "มีการเชื่อมโยงระหว่างที่อยู่กับบุคคลนี้อยู่ในระบบแล้ว",
+                ch: "",
+            },
+        })
     } else if (message.includes('UC_Customer_Person') && message.includes('duplicate key')) {
-        res.status(409).json({ status: 0, message: "Cannot insert duplicate value for customer and person" })
+        res.status(409).json({ 
+            status: 0, 
+            message: {
+                en: "Cannot insert duplicate value for customer and person",
+                th: "มีการเชื่อมโยงระหว่างลูกค้ากับบุคคลนี้อยู่ในระบบแล้ว",
+                ch: "",
+            },
+        })
     } else if (message.includes('UC_Person_Role') && message.includes('duplicate key')) {
-        res.status(409).json({ status: 0, message: "Cannot insert duplicate value for person and role" })
+        res.status(409).json({ 
+            status: 0, 
+            message: {
+                en: "Cannot insert duplicate value for person and role",
+                th: "มีการเชื่อมโยงระหว่างบุคคลกับตำแหน่งนี้อยู่ในระบบแล้ว",
+                ch: "",
+            },
+        })
     } else if (message.includes('UC_Fleet_Customer') && message.includes('duplicate key')) {
-        res.status(409).json({ status: 0, message: "Cannot insert duplicate value for customer and fleet" })
+        res.status(409).json({ 
+            status: 0, 
+            message: {
+                en: "Cannot insert duplicate value for customer and fleet",
+                th: "มีการเชื่อมโยงระหว่างลูกค้ากับกลุ่มยานยนต์นี้อยู่ในระบบแล้ว",
+                ch: "",
+            },
+        })
     } else {
-        res.status(500).json({ status: 0, message: "failed from server (by handleError middleware)", response: err })
+        res.status(500).json({ 
+            status: 0, 
+            message: {
+                en: "failed from server (by handleError middleware)",
+                th: "การประมวลผลเซิฟเวอร์ล้มเหลว (จาก middleware)",
+                ch: "",
+            },
+            response: err 
+        })
     }
 }
 
