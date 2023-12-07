8c045071fa9f5fec67baa64db13b110e50dbee9e
chonchanat <t.chonchanat@hotmail.com>
2023-12-07 09:18:46 +0700

    merge

--- src/operation/fleetOperation.ts
+++ src/operation/fleetOperation.ts
@@ -9,11 +9,11 @@ export async function getFleetTable(transaction: any, index: number, filter: str
         .query(`
             DECLARE @fleetTable IdType
             INSERT INTO @fleetTable
-            EXEC DevelopERP_ForTesting2..sp_filterFleet @fleet_name = @fleet_name, @customer_id = NULL, @vehicle_id = NULL, @firstIndex = @firstIndex, @lastIndex = @lastIndex
-            EXEC DevelopERP_ForTesting2..sp_formatFleetTable @fleetTable = @fleetTable, @firstIndex = @firstIndex
+            EXEC DevelopERP_Clear..sp_filterFleet @fleet_name = @fleet_name, @customer_id = NULL, @vehicle_id = NULL, @firstIndex = @firstIndex, @lastIndex = @lastIndex
+            EXEC DevelopERP_Clear..sp_formatFleetTable @fleetTable = @fleetTable, @firstIndex = @firstIndex
 
             SELECT COUNT(*) AS count_data
-            FROM DevelopERP_ForTesting2..Fleet
+            FROM DevelopERP_Clear..Fleet
             WHERE fleet_name LIKE @fleet_name AND parent_fleet_id IS NULL AND active = 1
         `)
 }
@@ -25,9 +25,9 @@ export async function getFleetChild(transaction: any, fleet_id: string) {
             DECLARE @fleetTable IdType
             INSERT INTO @fleetTable
             SELECT fleet_id
-            FROM DevelopERP_ForTesting2..Fleet
+            FROM DevelopERP_Clear..Fleet
             WHERE parent_fleet_id = @fleet_id
-            EXEC DevelopERP_ForTesting2..sp_formatFleetTable @fleetTable = @fleetTable, @firstIndex = 1
+            EXEC DevelopERP_Clear..sp_formatFleetTable @fleetTable = @fleetTable, @firstIndex = 1
         `)
 }
 
@@ -35,7 +35,7 @@ export async function getFleetName(transaction: any) {
     return await transaction.request()
         .query(`
             SELECT fleet_id, fleet_name
-            FROM DevelopERP_ForTesting2..Fleet
+            FROM DevelopERP_Clear..Fleet
             WHERE active = 1
             ORDER BY fleet_name
         `)
@@ -47,25 +47,25 @@ export async function getFleetData(transaction: any, fleet_id: string) {
         .query(`
             SELECT F.fleet_id, COALESCE(F.fleet_name, '') as fleet_name, 
                 PF.fleet_id AS parent_fleet_id, COALESCE(PF.fleet_name, '') AS parent_fleet_name
-            FROM DevelopERP_ForTesting2..Fleet F
-            LEFT JOIN DevelopERP_ForTesting2..Fleet PF
+            FROM DevelopERP_Clear..Fleet F
+            LEFT JOIN DevelopERP_Clear..Fleet PF
             ON F.parent_fleet_id = PF.fleet_id
             WHERE F.fleet_id = @fleet_id
 
             DECLARE @customerTable IdType
             INSERT INTO @customerTable
-            EXEC DevelopERP_ForTesting2..sp_filterCustomer @customer_name = '%',@fleet_id = @fleet_id, @person_id = NULL, @vehicle_id = NULL, @firstIndex = 0, @lastIndex = 0
-            EXEC DevelopERP_ForTesting2..sp_formatCustomerTable @customerTable = @customerTable, @firstIndex = 1
+            EXEC DevelopERP_Clear..sp_filterCustomer @customer_name = '%',@fleet_id = @fleet_id, @person_id = NULL, @vehicle_id = NULL, @firstIndex = 0, @lastIndex = 0
+            EXEC DevelopERP_Clear..sp_formatCustomerTable @customerTable = @customerTable, @firstIndex = 1
 
             DECLARE @personTable IdType
             INSERT INTO @personTable
-            EXEC DevelopERP_ForTesting2..sp_filterPerson @fullname = '%', @customer_id = NULL, @fleet_id = @fleet_id, @vehicle_id = NULL, @user_id = NULL, @firstIndex = 0, @lastIndex = 0
-            EXEC DevelopERP_ForTesting2..sp_formatPersonTable @personTable = @personTable, @firstIndex = 1
+            EXEC DevelopERP_Clear..sp_filterPerson @fullname = '%', @customer_id = NULL, @fleet_id = @fleet_id, @vehicle_id = NULL, @user_id = NULL, @firstIndex = 0, @lastIndex = 0
+            EXEC DevelopERP_Clear..sp_formatPersonTable @personTable = @personTable, @firstIndex = 1
 
             DECLARE @vehicleTable IdType
             INSERT INTO @vehicleTable 
-            EXEC DevelopERP_ForTesting2..sp_filterVehicle @license_plate = '%', @customer_id = NULL, @fleet_id = @fleet_id, @package_id = NULL, @firstIndex = 0, @lastIndex = 0
-            EXEC DevelopERP_ForTesting2..sp_formatVehicleTable @vehicleTable = @vehicleTable, @firstIndex = 1
+            EXEC DevelopERP_Clear..sp_filterVehicle @license_plate = '%', @customer_id = NULL, @fleet_id = @fleet_id, @package_id = NULL, @firstIndex = 0, @lastIndex = 0
+            EXEC DevelopERP_Clear..sp_formatVehicleTable @vehicleTable = @vehicleTable, @firstIndex = 1
         `)
 }
 
@@ -75,7 +75,7 @@ export async function deleteFleet(transaction: any, fleet_id: string, action_by:
         .input('action_by', sql.INT, action_by)
         .input('action_date', sql.DATETIME, datetime)
         .query(`
-            EXEC DevelopERP_ForTesting2..sp_delete_fleet @fleet_id = @fleet_id, 
+            EXEC DevelopERP_Clear..sp_delete_fleet @fleet_id = @fleet_id, 
                 @action_by = @action_by, @action_date = @action_date
         `)
 }
@@ -87,7 +87,7 @@ export async function createFleetNew(transaction: any, fleet: Fleet, action_by: 
         .input('action_by', sql.INT, action_by)
         .input('action_date', sql.DATETIME, datetime)
         .query(`
-        EXEC DevelopERP_ForTesting2..sp_insert_fleet @fleet_name = @fleet_name, @parent_fleet_id = @parent_fleet_id,
+        EXEC DevelopERP_Clear..sp_insert_fleet @fleet_name = @fleet_name, @parent_fleet_id = @parent_fleet_id,
             @action_by = @action_by, @action_date = @action_date
     `)
 }
@@ -100,7 +100,7 @@ export async function updateFleet(transaction: any, fleet_id: string | number, f
         .input('action_by', sql.INT, action)
         .input('action_date', sql.DATETIME, datetime)
         .query(`
-        EXEC DevelopERP_ForTesting2..sp_update_fleet @fleet_id = @fleet_id, @fleet_name = @fleet_name,
+        EXEC DevelopERP_Clear..sp_update_fleet @fleet_id = @fleet_id, @fleet_name = @fleet_name,
             @parent_fleet_id = @parent_fleet_id, @action_by = @action_by, @action_date = @action_date
     `)
 }
